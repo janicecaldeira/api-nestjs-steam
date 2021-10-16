021a5963d3a6db60cf506e4f35f81fd29d2ead8c
janicecaldeira <janicecaldeira@gmail.com>
2021-10-15 22:30:07 -0300

    Atualizando auth e user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { CredentialsDto } from './credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../users/user.entity';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +32,11 @@ export class AuthController {
     @Body(ValidationPipe) credentialsDto: CredentialsDto,
   ): Promise<{ token: string }> {
     return await this.authService.signIn(credentialsDto);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  getMe(@GetUser() user: User): User {
+    return user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
-    PassportModule.register({ defaultStrategy: 'jwt ' }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'super-secret',
       signOptions: {
