b4bc24f37bfa234f79a20508776e568bd7f6ca8f
janicecaldeira <janicecaldeira@gmail.com>
2021-10-20 21:37:55 -0300

    Envio de e-mail

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,19 +10,38 @@ import { CredentialsDto } from './credentials.dto';
 import { User } from '../users/user.entity';
 import { UserRole } from 'src/users/user-roles.enum';
 import { JwtService } from '@nestjs/jwt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(UserRepository) private userRepository: UserRepository,
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
     private jwtService: JwtService,
+    private mailerService: MailerService,
   ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
     if (createUserDto.password != createUserDto.passwordConfirmation) {
       throw new UnprocessableEntityException('As senhas não conferem');
     } else {
-      return this.userRepository.createUser(createUserDto, UserRole.USER);
+      const user = await this.userRepository.createUser(
+        createUserDto,
+        UserRole.USER,
+      );
+
+      const mail = {
+        to: user.email,
+        from: 'noreply@steam.com',
+        subject: 'E-mail de confirmação',
+        template: 'email-confirmation',
+        context: {
+          token: user.confirmationToken,
+        },
+      };
+
+      await this.mailerService.sendMail(mail);
+      return user;
     }
   }
 
