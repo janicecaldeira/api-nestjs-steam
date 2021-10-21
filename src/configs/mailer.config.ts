bd737874824f5e78eded890e8e7fa529e94c657b
janicecaldeira <janicecaldeira@gmail.com>
2021-10-21 17:38:48 -0300

    Configurando dotenv

--- src/configs/mailer.config.ts
+++ src/configs/mailer.config.ts
@@ -1,7 +1,9 @@
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import * as path from 'path';
-import process from 'process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const mailerConfig: MailerOptions = {
   template: {
