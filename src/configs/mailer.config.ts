c1f6261665fa083e23f6be1b51ac4a356f72e4ca
janicecaldeira <janicecaldeira@gmail.com>
2021-10-21 19:56:04 -0300

    Correções

--- src/configs/mailer.config.ts
+++ src/configs/mailer.config.ts
@@ -1,9 +1,6 @@
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import * as path from 'path';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const mailerConfig: MailerOptions = {
   template: {
